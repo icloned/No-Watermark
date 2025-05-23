92c6e5c89c3db23b907ec7e9406a76dbac1e4269
icloned <125537875+icloned@users.noreply.github.com>
2025-05-23 10:27:25 +0800

    Update content.js

--- content.js
+++ content.js
@@ -60,8 +60,8 @@ function processImages(images, siteId) { // siteId is needed for addDownloadButt
 
 // 根据图片尺寸处理
 function processImageSize(img, siteId) { // siteId is needed for addDownloadButton
-  // 检查图片宽度是否大于等于600px
-  if (img.naturalWidth >= 600 || img.width >= 600 || img.offsetWidth >= 600) {
+  // 检查图片宽度是否大于等于200px
+  if (img.naturalWidth >= 200 || img.width >= 200 || img.offsetWidth >= 200) {
     // 标记图片已处理
     img.setAttribute('data-processed', 'true');
     
